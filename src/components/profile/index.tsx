8eb021ee91f718788c05cb7df220c30a7f4c61c3
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-06-04 10:57:26 -0300

    feat(profileImage)

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 import styles from "./profile.module.css"
 import { url } from "inspector";
+import { RiPencilFill } from "react-icons/ri";
+import { BsXLg } from "react-icons/bs";
+import { IoCheckmark } from "react-icons/io5";
+import { FaUserNinja } from "react-icons/fa";
 
 interface profileButtonProps{
     
@@ -38,12 +42,12 @@ function ProfileImage(){
             }
         onClick={addImage}
             >
-            <img src= "/assets/confirmEdit.svg"></img>
+            <IoCheckmark style={{width: "58%", height: "58%"}}  />
         </button>
 
        <button className={styles.button} onClick={removeImage}>
         {
-            isEditing? <img src= "/assets/cancelEdit.svg"/> : <img src={"/assets/editPencil.svg"} /> 
+            isEditing? <BsXLg style={{width: "58%", height: "58%"}} /> : <RiPencilFill style={{width: "58%", height: "58%"}}/>
         }
        </button>
     </div>
--- src/pages/profileImage.tsx
+++ src/pages/profileImage.tsx
@@ -1,7 +1,7 @@
 import { ProfileImage } from "@/components/profile"
 import styles from "../styles/pages/profileImage.module.css"
 
-export default function profile(){
+export default function ProfileImg(){
     return <>
             <ProfileImage/>
     </>
