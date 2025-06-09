2e4ca93109d6a8322a88aaa8fc416ad71f0e8270
oTyR3D <filipefonseca420@gmail.com>
2025-06-09 11:11:13 -0300

    feat: Adicição dos outros elementos criados para a pagina de perfil

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,57 +5,62 @@ import ReverseButton from "@/components/buttons/reverse-button";
 import minhaImagem from "../../../public/assets/kakashi-malvezzi.png";
 import { GiStarShuriken } from "react-icons/gi";
 import { TextArea } from "@/components/inputs/text-area";
+import SideBar from "@/components/vertical_bar/bar_function"
+import { Logotipo } from "@/components/logo";
+import modificarFotoIcon from "../../../public/assets/iconeEditarFoto.svg"
 
 export default function TelaProfile() {
   return (
     <div className={styles.body}>
-      
-        <div>
-          <p>1</p>
-          <p>2</p>
-          <p>3</p>
-        </div>
-        <span>asd</span>
-      </div>
-      <div className={styles.white_box}> 
-        <div className={styles.logo_place}><p>teste</p></div>{/*SUBSTITUIR ESSA DIV PELO COMPONENTE "LOGOTIPO OBS: NO CSS TAMBÉM"*/}
-      <div className={styles.container}>
-        <div className={styles.profile_box}>
-          <div className={styles.left}>
-            <img src={minhaImagem.src} alt="Imagem Teste" /> {/*Substituir pela foto funcional e remover o css dessa*/}
-            <div className={styles.textinputs}>
-              <div className={styles.inputs}>
-                <span>Nickname</span>
-                <TextInput />
+      <SideBar />
+      <div className={styles.white_box}>
+        <Logotipo />
+        <div className={styles.container}>
+          <div className={styles.profile_box}>
+            <div className={styles.left}>
+              <div>
+                <img src={minhaImagem.src} alt="Imagem Teste" />
+                <div>
+                  <img src="/assets/iconeEditarFoto.svg" alt="Icone para editar a foto" className={styles.editarFoto}/>
+                  
+                  {/* TEM QUE ARRUMAR O TAMANHO DO ICONE*/}
+
+                </div>
               </div>
-              <div className={styles.inputs}>
-                <span>Nome</span>
-                <TextInput />
-              </div>
-              <div className={styles.inputs}>
-                <span>Email</span>
-                <TextInput />
+              <div className={styles.textinputs}>
+                <div className={styles.inputs}>
+                  <span>Nickname</span>
+                  <TextInput />
+                </div>
+                <div className={styles.inputs}>
+                  <span>Nome</span>
+                  <TextInput />
+                </div>
+                <div className={styles.inputs}>
+                  <span>Email</span>
+                  <TextInput />
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.mid}><hr /><GiStarShuriken className={styles.shuriken_icon}/> <hr /></div>
-          <div className={styles.right}>
+            <div className={styles.mid}><hr /><GiStarShuriken className={styles.shuriken_icon} /> <hr /></div>
+            <div className={styles.right}>
               <div>
                 <span>Bio</span>
                 <TextArea />
               </div>
               <div className={styles.buttons}>
                 <div className={styles.box}>
-                  <DefaultButton nameInside="Salvar"/>
+                  <DefaultButton nameInside="Salvar" />
                 </div>
                 <div className={styles.box}>
-                  <ReverseButton texto="Descartar"/>
+                  <ReverseButton texto="Descartar" />
                 </div>
               </div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
     </div>
+
   );
 }
